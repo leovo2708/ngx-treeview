47f4f28fb0278e8c15bab3e0e73a567ffb929452
developingdenny <dkruep@sbcglobal.net>
2019-01-20 18:32:26 -0600

    add checked interpretation to treeview pipe

--- src/lib/treeview.pipe.spec.ts
+++ src/lib/treeview.pipe.spec.ts
@@ -22,4 +22,43 @@ describe('TreeviewPipe', () => {
         expect(objects[0].name === treeItems[0].text && objects[1].name === treeItems[1].text).toBe(true, 'validate text');
         expect(objects[0] === treeItems[0].value && objects[1] === treeItems[1].value).toBe(true, 'validate value');
     });
+
+    it('transforms a list of objects to list of TreeItem objects that include checked property', () => {
+        const objects: any[] = [{
+            name: 'leo',
+            age: '18',
+            checked: true
+        }, {
+            name: 'vo',
+            age: '14',
+            checked: false
+        }];
+
+        const treeItems = pipe.transform(objects, 'name');
+        expect(objects.length === treeItems.length).toBe(true, 'same length');
+        expect(objects[0].name === treeItems[0].text && objects[1].name === treeItems[1].text).toBe(true, 'validate text');
+        expect(objects[0] === treeItems[0].value && objects[1] === treeItems[1].value).toBe(true, 'validate value');
+        expect(objects[0].checked === treeItems[0].value.checked
+            && objects[1].checked === treeItems[1].value.checked).toBe(true, 'validate checked');
+    });
+
+    it('only includes checked property when provided', () => {
+        const objects: any[] = [{
+            name: 'leo',
+            age: '18',
+        }, {
+            name: 'vo',
+            age: '14',
+            checked: false
+        }];
+
+        const treeItems = pipe.transform(objects, 'name');
+        expect(objects.length === treeItems.length).toBe(true, 'same length');
+        expect(objects[0].name === treeItems[0].text && objects[1].name === treeItems[1].text).toBe(true, 'validate text');
+        expect(objects[0] === treeItems[0].value && objects[1] === treeItems[1].value).toBe(true, 'validate value');
+        expect(objects[0].checked === treeItems[0].value.checked
+            && objects[1].checked === treeItems[1].value.checked).toBe(true, 'validate checked');
+        expect(treeItems[1].checked).toBe(false);
+    });
+
 });
