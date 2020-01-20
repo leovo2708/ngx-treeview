d1972fe7c85fbde920fce6fb886d00e207b218a9
Arnold <arnold.gabriel@sgs.com>
2020-01-20 13:50:20 +0000

    Fixed demo.

--- src/demo/product/product.component.ts
+++ src/demo/product/product.component.ts
@@ -25,7 +25,7 @@ export class ProductTreeviewConfig extends TreeviewConfig {
     ]
 })
 export class ProductComponent implements OnInit {
-    @ViewChild(TreeviewComponent) treeviewComponent: TreeviewComponent;
+    @ViewChild(TreeviewComponent, { static: false }) treeviewComponent: TreeviewComponent;
     items: TreeviewItem[];
     rows: string[];
 
