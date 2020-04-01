b4f593e8d47188f105826a10d9d93287f9c54856
Rabi Róbert <ingenmaffen@gmail.com>
2020-04-01 15:27:14 +0200

    Fix package on angular 9

--- src/lib/dropdown-treeview.component.ts
+++ src/lib/dropdown-treeview.component.ts
@@ -1,44 +1,56 @@
-﻿import { Component, EventEmitter, Input, Output, HostListener, ViewChild, TemplateRef } from '@angular/core';
-import { TreeviewI18n } from './treeview-i18n';
-import { TreeviewItem } from './treeview-item';
-import { TreeviewConfig } from './treeview-config';
-import { TreeviewComponent } from './treeview.component';
-import { DropdownDirective } from './dropdown.directive';
-import { TreeviewHeaderTemplateContext } from './treeview-header-template-context';
-import { TreeviewItemTemplateContext } from './treeview-item-template-context';
+﻿import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  HostListener,
+  ViewChild,
+  TemplateRef
+} from "@angular/core";
+import { TreeviewI18n } from "./treeview-i18n";
+import { TreeviewItem } from "./treeview-item";
+import { TreeviewConfig } from "./treeview-config";
+import { TreeviewComponent } from "./treeview.component";
+import { DropdownDirective } from "./dropdown.directive";
+import { TreeviewHeaderTemplateContext } from "./treeview-header-template-context";
+import { TreeviewItemTemplateContext } from "./treeview-item-template-context";
 
 @Component({
-    selector: 'ngx-dropdown-treeview',
-    templateUrl: './dropdown-treeview.component.html',
-    styleUrls: ['./dropdown-treeview.component.scss']
+  selector: "ngx-dropdown-treeview",
+  templateUrl: "./dropdown-treeview.component.html",
+  styleUrls: ["./dropdown-treeview.component.scss"]
 })
 export class DropdownTreeviewComponent {
-    @Input() buttonClass = 'btn-outline-secondary';
-    @Input() headerTemplate: TemplateRef<TreeviewHeaderTemplateContext>;
-    @Input() itemTemplate: TemplateRef<TreeviewItemTemplateContext>;
-    @Input() items: TreeviewItem[];
-    @Input() config: TreeviewConfig;
-    @Output() selectedChange = new EventEmitter<any[]>(true);
-    @Output() filterChange = new EventEmitter<string>();
-    @ViewChild(TreeviewComponent) treeviewComponent: TreeviewComponent;
-    @ViewChild(DropdownDirective) dropdownDirective: DropdownDirective;
+  @Input() buttonClass = "btn-outline-secondary";
+  @Input() headerTemplate: TemplateRef<TreeviewHeaderTemplateContext>;
+  @Input() itemTemplate: TemplateRef<TreeviewItemTemplateContext>;
+  @Input() items: TreeviewItem[];
+  @Input() config: TreeviewConfig;
+  @Output() selectedChange = new EventEmitter<any[]>(true);
+  @Output() filterChange = new EventEmitter<string>();
+  @ViewChild(TreeviewComponent, { static: true })
+  treeviewComponent: TreeviewComponent;
+  @ViewChild(DropdownDirective, { static: true })
+  dropdownDirective: DropdownDirective;
 
-    constructor(
-        public i18n: TreeviewI18n,
-        private defaultConfig: TreeviewConfig
-    ) {
-        this.config = this.defaultConfig;
-    }
+  constructor(
+    public i18n: TreeviewI18n,
+    private defaultConfig: TreeviewConfig
+  ) {
+    this.config = this.defaultConfig;
+  }
 
-    getText(): string {
-        return this.i18n.getText(this.treeviewComponent.selection);
-    }
+  getText(): string {
+    return this.treeviewComponent && this.treeviewComponent.selection
+      ? this.i18n.getText(this.treeviewComponent.selection)
+      : "";
+  }
 
-    onSelectedChange(values: any[]) {
-        this.selectedChange.emit(values);
-    }
+  onSelectedChange(values: any[]) {
+    this.selectedChange.emit(values);
+  }
 
-    onFilterChange(text: string) {
-      this.filterChange.emit(text);
-    }
+  onFilterChange(text: string) {
+    this.filterChange.emit(text);
+  }
 }
